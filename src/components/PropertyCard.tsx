5082ad99df1df22776392313102d82c690c7b5af
deddybrama11 <dedybramayadi@gmail.com>
2023-11-20 00:37:38 +0700

    Add section Property collection

---
+++ src/components/PropertyCard.tsx
@@ -0,0 +1,72 @@
+import { Text, Box, Flex, Image } from "@chakra-ui/react";
+
+interface IPropertyCard {
+  mainImgUrl: string;
+  imgListUrl: string[];
+  title: string;
+  location: string;
+  totalInvestor: number;
+}
+
+const PropertyCard = (props: IPropertyCard) => {
+  const { imgListUrl, location, mainImgUrl, title, totalInvestor } = props;
+
+  return (
+    <Box>
+      <Box
+        p={5}
+        background={"#57467275"}
+        borderRadius={"20px"}
+        border={"2px solid #A4A4BE"}
+      >
+        <Box overflow={"hidden"} borderRadius={"20px"} marginBottom={2}>
+          <Image src={mainImgUrl} alt="" />
+        </Box>
+        <Flex gap={3}>
+          {imgListUrl.map((url, i) => (
+            <Box flex={1} key={i}>
+              <Image
+                src={url}
+                alt="villa-img"
+                borderRadius={10}
+                height={"100%"}
+              />
+            </Box>
+          ))}
+        </Flex>
+      </Box>
+      <Box
+        p={5}
+        background={"#57467275"}
+        borderRadius={"20px"}
+        border={"2px solid #A4A4BE"}
+        marginTop={4}
+      >
+        <Flex justifyContent={"space-between"}>
+          <Box>
+            <Text mb={1}>{title}</Text>
+            <Flex gap={2} alignItems={"self-end"}>
+              <Image
+                src="icons/pinpoint.png"
+                objectFit={"contain"}
+                alt="pin point"
+              />
+              <Text fontSize={12}>{location}</Text>
+            </Flex>
+          </Box>
+          <Flex
+            background={"#57467275"}
+            borderRadius={"20px"}
+            border={"2px solid #A4A4BE"}
+            px={3}
+            alignItems={"center"}
+          >
+            Total {totalInvestor} Investor
+          </Flex>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default PropertyCard;
---
+++ src/components/SectionProperty.tsx
@@ -0,0 +1,51 @@
+import { useTranslation } from "react-i18next";
+import { VStack, Text, Box, Flex, Image, Container } from "@chakra-ui/react";
+import PropertyCard from "./PropertyCard";
+
+const property = {
+  mainImgUrl: "villa.jpg",
+  imgListUrl: ["villa.jpg", "villa2.jpg", "villa3.jpg"],
+  title: "Villa Ubud",
+  location: "Ubud, Bali, Indonesia",
+  totalInvestor: 100,
+};
+
+const SectionProperty = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Container maxW={"container.xl"}>
+      <VStack py={10}>
+        <Text>{t("pages.home.propertySection.title")}</Text>
+        <Text fontSize={28} fontWeight={"800"} marginBottom={8}>
+          {t("pages.home.propertySection.subTitle")}
+        </Text>
+        <Flex gap={5}>
+          <PropertyCard
+            imgListUrl={property.imgListUrl}
+            location={property.location}
+            mainImgUrl={property.mainImgUrl}
+            title={property.title}
+            totalInvestor={property.totalInvestor}
+          />
+          <PropertyCard
+            imgListUrl={property.imgListUrl}
+            location={property.location}
+            mainImgUrl={property.mainImgUrl}
+            title={property.title}
+            totalInvestor={property.totalInvestor}
+          />
+          <PropertyCard
+            imgListUrl={property.imgListUrl}
+            location={property.location}
+            mainImgUrl={property.mainImgUrl}
+            title={property.title}
+            totalInvestor={property.totalInvestor}
+          />
+        </Flex>
+      </VStack>
+    </Container>
+  );
+};
+
+export default SectionProperty;
